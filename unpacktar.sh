de8ee55437dce83f29067582992dd1d6848924cb
kiwiheretic <kiwiheretic@myself.com>
2016-11-25 06:26:36 +0000

    Fix unpacktar.sh

--- unpacktar.sh
+++ unpacktar.sh
@@ -2,11 +2,11 @@
 # designed for ubuntu 16.04 DigitalOcean install
 archive=$1
 domain=$2
-rm -fr ~/$domain/*
-if [ -z "$archive" ] || [ -z "$domain" ]; then
+if [ -z "$archive" -o -z "$domain" ]; then
     echo "usage: unpacktar <archive> <domain>"
-    return
+    exit
 fi
+rm -fr ~/$domain/*
 ( cd ~/$domain && tar -xvzf ~/${archive} --strip-components=1 && \
 chown -R www-data.www-data * && chmod -R g+rw * )
 chown www-data.www-data ~/$domain
