2278baf0c2a9f3f215383c363c2931609e485f2f
kiwiheretic <kiwiheretic@myself.com>
2018-07-03 02:13:11 +0000

    Add enable and disable commands to vhost.sh

--- vhost.sh
+++ vhost.sh
@@ -6,9 +6,11 @@ shopt -s nullglob # enable
 # Get the path of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+TEMPLATEDIR="/home/splat/bash-scripts/nginx-templates"
+
 if [ "$1 $2" = "templates list" ]; then
     echo -e "Available Templates\n"
-    files=( $DIR/nginx-templates/*.template )
+    files=( $TEMPLATEDIR/*.template )
     for f in "${files[@]}"
     do
         # Remove the pathname
@@ -27,18 +29,22 @@ elif [ "$1" = "create" ]; then
         echo "please supply a domain name"
         exit 1
     fi
-    if [ ! -f  "$DIR/nginx-templates/$2.template" ]; then
+    if [ ! -f  "$TEMPLATEDIR/$2.template" ]; then
         echo "No such template $2"
         exit 1
     fi
-    cat "$DIR/nginx-templates/$2.template" | sed -e "s/domain\.com/$3/g" > "/etc/nginx/sites-available/$3.conf"
+    if [ -f  "/etc/nginx/sites-available/$3.conf" ]; then
+        echo "vhost for $3 already exists"
+        exit 1
+    fi
+    cat "$TEMPLATEDIR/$2.template" | sed -e "s/domain\.com/$3/g" > "/etc/nginx/sites-available/$3.conf"
     if [ ! -d "/usr/share/nginx/html/$3/" ]; then
         mkdir  "/usr/share/nginx/html/$3/"
     fi
-	ln -s "/usr/share/nginx/html/$3/"  "~/$3"
+	ln -s "/usr/share/nginx/html/$3/"  "$HOME/$3"
 	ln -s "/etc/nginx/sites-available/$3.conf"  "/etc/nginx/sites-enabled/$3.conf"
-    service nginx reload
     echo "vhost $domain created and enabled"
+    echo "remember to reload nginx with \"sudo service nginx reload\""
 elif [ "$1" = "destroy" ]; then
     if [ -z "$2" ]; then
         echo "No domain supplied"
@@ -52,6 +58,39 @@ elif [ "$1" = "destroy" ]; then
     rm "/etc/nginx/sites-enabled/ssl/$2.conf" 2>/dev/null
     rm "/etc/nginx/sites-available/$2.conf"
     rm -fr "/usr/share/nginx/html/$2/"
-    rm "~/$2"
+    rm "$HOME/$2"
 
+    echo "vhost $domain destroyed"
+    echo "remember to reload nginx with \"sudo service nginx reload\""
+elif [ "$1" = "disable" ]; then
+    if [ -z "$2" ]; then
+        echo "No domain supplied"
+        exit 1
+    fi
+    if [ ! -L "/etc/nginx/sites-enabled/$2.conf" ]; then
+        echo "vhost symlink for domain $2 does not exist - no action taken"
+        exit 1
+    fi
+    rm "/etc/nginx/sites-enabled/$2.conf"
+
+    echo "symlink for vhost $domain deleted"
+    echo "remember to reload nginx with \"sudo service nginx reload\""
+elif [ "$1" = "enable" ]; then
+    if [ -z "$2" ]; then
+        echo "No domain supplied"
+        exit 1
+    fi
+    if [ -L "/etc/nginx/sites-enabled/$2.conf" ]; then
+        echo "vhost symlink for domain $2 already exists - no action taken"
+        exit 1
+    fi
+    if [ ! -f "/etc/nginx/sites-available/$2.conf" ]; then
+        echo "vhost entry for domain $2 missing - no action taken"
+        echo "suggest recreating the vhost to fix"
+        exit 1
+    fi
+    ln -s "/etc/nginx/sites-available/$2.conf" "/etc/nginx/sites-enabled/$2.conf"
+
+    echo "symlink for vhost $domain created"
+    echo "remember to reload nginx with \"sudo service nginx reload\""
 fi
